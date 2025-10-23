407464996d5a267e3115d0d993c85f4c5699c135
broncosfan915 <johnlewis092@gmail.com>
2025-10-23 11:34:01 -0400

    Utilized accelerate for distributed inference

--- demo.py
+++ demo.py
@@ -1,20 +1,23 @@
 import torch
+from accelerate import Accelerator
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# Initialize accelerator for distributed inference
+accelerator = Accelerator()
 
 # Initialize Hugging Face model and tokenizer
 model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2-medium")
 tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2-medium")
 
+# Prepare model for distributed inference
+model = accelerator.prepare(model)
+
 # Input prompt and tokenize
 prompt = input("Input your prompt: ")
 tokenizedPrompt = tokenizer(prompt, return_tensors='pt')
 
-# Initialize device using CUDA cores and maps model to device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
 # copy input tensor to CUDA
-tokenizedPrompt = {k: v.to(device) for k, v in tokenizedPrompt.items()}
+tokenizedPrompt = {k: v.to(accelerator.device) for k, v in tokenizedPrompt.items()}
 
 # run inference
 generated = model.generate(**tokenizedPrompt)
